f2b4c5bd2a0f4e4a0cdf14295f685284086b4854
benoitdesrosiers <desrosib@cegepdrummond.ca>
2016-10-20 15:00:51 -0400

    ajout du test de création du modèle personne

---
+++ tests/DemoBDPersonneTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Models\Personne;
+
+
+class DemoBDPersonneTest extends TestCase
+{
+	use DatabaseTransactions;
+	
+    /** @test */
+    public function sauvegarde_simple_du_modele()
+    {
+        $personne = new Personne;
+        $personne->nom = "Nom Valide";
+        $personne->dateNaissance = '2000-12-25';
+        $personne->telephone = "123-123-1234";
+        $this->assertTrue($personne->validate(),'la personne de base ne valide pas');
+        $this->assertTrue($personne->save());
+        $this->seeInDatabase('personnes', [
+        		'id'=>$personne->id,
+        		'nom'=>$personne->nom,
+        		'dateNaissance'=>$personne->dateNaissance,
+        		'telephone'=>$personne->telephone
+        ]);
+        
+    }
+}
